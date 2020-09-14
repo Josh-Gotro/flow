1b2aef3a39e6f4791c617af137c34641c78a3d01
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-09-13 19:06:24 -0500

    cleanup react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let stravaID = 53580
+  let auth = `http://www.strava.com/oauth/authorize?client_id=${stravaID}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read`
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
